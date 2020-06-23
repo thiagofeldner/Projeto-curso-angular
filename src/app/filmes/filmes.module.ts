bb3f8cd749deb14ffd6836ed2a0e27286a6d2cb9
Thiago feldner <tfeldner@hotmail.com>
2020-06-23 12:25:07 -0300

    Primeiro commit do curso de angular da Digital Innovatio One.

---
+++ src/app/filmes/filmes.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+import { CadastroFilmesComponent } from './cadastro-filmes/cadastro-filmes.component';
+import { MaterialModule } from '../shared/material/material.module';
+import { ListagemFilmesComponent } from './listagem-filmes/listagem-filmes.component';
+import { VisualizarFilmesComponent } from './visualizar-filmes/visualizar-filmes.component';
+import { CamposModule } from '../shared/components/campos/campos.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CamposModule,
+    InfiniteScrollModule,
+  ],
+  
+  declarations: [CadastroFilmesComponent, ListagemFilmesComponent, VisualizarFilmesComponent]
+})
+export class FilmesModule { }
